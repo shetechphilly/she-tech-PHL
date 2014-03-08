badd2dd00f0ae6528592d86d64d5add95ec54051
Catherine Farman <cfarman@gmail.com>
2014-03-08 17:44:01 -0500

    add livereload to project

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt){
         },
         files: {
           // Where to combine and minify JS files, followed by list of which files to include and exclude
-          'a/j/script.min.js' : ['a/j/plugins/*.js', 'a/j/script.js', '!a/j/plugins/modernizr.js', '!a/j/plugins/livereload.js']
+          'js/script.min.js' : ['js/plugins/*.js', 'js/script.js', '!js/plugins/modernizr.js', '!js/plugins/livereload.js']
         }
     },
 
@@ -18,9 +18,9 @@ module.exports = function(grunt){
       dynamic: {                         // Another target
         files: [{
           expand: true,                  // Enable dynamic expansion
-          cwd: 'a/i/',                   // cwd is 'current working directory' - Src matches are relative to this path
+          cwd: 'images',                   // cwd is 'current working directory' - Src matches are relative to this path
           src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-          dest: 'a/i/'                  // Destination path prefix
+          dest: 'images'                  // Destination path prefix
         }]
       }
     },
@@ -31,11 +31,11 @@ module.exports = function(grunt){
         livereload: true
       },
       js: {
-        files: ['a/j/**/*.js', '!a/j/script.min.js'], // Watch for changes in JS files except for script.min.js to avoid reload loops
+        files: ['js/**/*.js', '!js/script.min.js'], // Watch for changes in JS files except for script.min.js to avoid reload loops
         tasks: ['uglify']
       },
       images: {
-        files: ['**/*.{png,jpg,gif}'],
+        files: ['images/*.{png,jpg,gif}'],
         tasks: ['imagemin']
       }
 		},
